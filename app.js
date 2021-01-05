eaccb47f6013d2aa70eff5f438736d3a29ef3fae
Matias Melian <Matias Melian>
2021-01-05 19:18:01 -0300

    adding files

--- app.js
+++ app.js
@@ -17,12 +17,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
-/* app.listen(3030, () => { 
+app.listen(process.env.PORT || 3000, () => { 
     console.log('-----------------------------------------------')
-    console.log('¡Todo joya! Ya podés ir a http://localhost:3030');
+    console.log('Todo salio OK! Ya podés ir a http://localhost:3000');
     console.log('-----------------------------------------------')
-}) */
-
-app.listen(process.env.port || 3000, function() {
-    console.log('Servidor Corriendo')
-})
+});
