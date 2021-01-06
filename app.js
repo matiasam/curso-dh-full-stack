69eba4148b0c89ca7261e78d5bc983f748f13e98
Matias Melian <Matias Melian>
2021-01-06 17:54:15 -0300

    adding files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* const path = require('path');
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -16,30 +16,6 @@ app.get('/', (req, res) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/register.html'));
 });
-
-app.listen(process.env.PORT || 3000, () => { 
-    console.log('-----------------------------------------------')
-    console.log('Todo salio OK! Ya podés ir a http://localhost:3000');
-    console.log('-----------------------------------------------')
-}); */
-
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-
-
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
-});
-
-app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
-});
-
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
-});
-
 
 app.listen(process.env.PORT || 3000, () => { 
     console.log('-----------------------------------------------')
